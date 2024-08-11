069a54b4a9228718a12cb4e0fb56758b4951ad19
Sanket Koirala <koiralasanket11@gmail.com>
2024-08-11 16:40:46 -0500

    App module changed to fit in new final grade component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { FinalGradeCalculatorComponent } from './final-grade-calculator/final-grade-calculator.component';
 
 
 
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    FinalGradeCalculatorComponent
 
   ],
   imports: [
