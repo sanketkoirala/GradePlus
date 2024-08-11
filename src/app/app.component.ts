da3b6f2db40f2417cf8dd2b39dd2deac084b5629
Sanket Koirala <koiralasanket11@gmail.com>
2024-08-11 15:34:56 -0500

    App component to display home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GradePlus';
+export class AppComponent implements OnInit {
+title = 'web-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
 }
